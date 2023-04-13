8fb552222b0948921b95ffb68074d638b81de70c
Rolando Arellano <rolandoarellano9869@hotmail.com>
2023-04-13 15:52:44 -0700

    Resolve error sidebar v1

--- src/components/Sidebars.jsx
+++ src/components/Sidebars.jsx
@@ -9,16 +9,18 @@ const Sidebars = () => {
       <div>
         <h1 className='text-center text-2xl font-bold mb-10 text-gray-300'>Admin<span className='text-primary text-4xl'>.</span>
         </h1>
-        <ul className='text-mb'>
+
+        <ul>
           <li>
             <Link to="/admin/users " className=' flex items-center gap-4 py-2 px-4 rounded-lg hover:bg-secondary-900 transition-colors'><RiBarChartFill className='text-primary' />Analíticas</Link>
           </li>
-
           <li>
             <Link to="/admin/users " className=' flex items-center gap-4 py-2 px-4 rounded-lg hover:bg-secondary-900 transition-colors'><RiEarthFill className='text-primary' />Red sociales</Link>
-            <ul className='mt-4 '>
-              <li> 
-                <Link to="/" className='py-2 px-4 border-l border-gray-500 ml-6 block relative before:w-3 before:h-3 before:aboslute before:bg-primary before:rounded-full before:-left-2 before:top-1/2 before:-translate-y-1/2 before:border-2 before:border-secondary-100'>Post Red Social</Link>
+
+            {/* submenu           */}
+            <ul className='mt-4 text-xs'>
+              <li>
+                <Link to="/" className='py-2 px-4 border-l border-gray-500 ml-6 block relative before:w-3 before:h-3 before:absolute before:bg-primary before:rounded-full before:-left-2 before:top-1/2 before:-translate-y-1/2 before:border-2 before:border-secondary-100'>Post Red Social</Link>
               </li>
               <li>
                 <Link to="/" className='py-2 px-4 border-l border-gray-500 ml-6 block  '>Estadisticas</Link>
@@ -27,8 +29,11 @@ const Sidebars = () => {
                 <Link to="/" className='py-2 px-4 border-l border-gray-500 ml-6 block'>Perfiles</Link>
               </li>
             </ul>
+
           </li>
 
+      
+          {/* submenu */}
           <li>
             <Link to="/admin/users " className=' flex items-center gap-4 py-2 px-4 rounded-lg hover:bg-secondary-900 transition-colors'><RiMessage2Fill className='text-primary' />Mensajes</Link>
           </li>
@@ -42,7 +47,7 @@ const Sidebars = () => {
       <nav className=' text-mb '>
         <Link to="/admin/users " className=' flex items-center gap-4 py-2 px-4 rounded-lg hover:bg-secondary-900 transition-colors'><RiLogoutCircleRLine className='text-primary' />Cerrar sesión</Link>
       </nav>
-    </div>
+    </div >
   )
 }
 
