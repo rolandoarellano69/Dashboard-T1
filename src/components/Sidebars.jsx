8932d62aa77ba27996e35647494bcacb357e1027
Rolando Arellano <rolandoarellano9869@hotmail.com>
2023-04-14 15:22:28 -0700

    Updates for SubList v1

--- src/components/Sidebars.jsx
+++ src/components/Sidebars.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 //icons
-import { RiBarChartFill, RiEarthFill, RiMessage2Fill, RiCalendarTodoFill, RiLogoutCircleRLine } from 'react-icons/ri'
+import { RiBarChartFill, RiEarthFill, RiMessage2Fill, RiCalendarTodoFill, RiLogoutCircleRLine, RiArrowDropDownLine, RiArrowRightSLine } from 'react-icons/ri'
 
 const Sidebars = () => {
   return (
@@ -12,27 +12,36 @@ const Sidebars = () => {
 
         <ul>
           <li>
-            <Link to="/admin/users " className=' flex items-center gap-4 py-2 px-4 rounded-lg hover:bg-secondary-900 transition-colors'><RiBarChartFill className='text-primary' />Analíticas</Link>
+            <Link to="/admin/users " className=' flex items-center gap-4 py-2 px-4 rounded-lg hover:bg-secondary-900 transition-colors'>
+
+
+              <RiBarChartFill className='text-primary' />Analíticas</Link>
           </li>
           <li>
-            <Link to="/admin/users " className=' flex items-center gap-4 py-2 px-4 rounded-lg hover:bg-secondary-900 transition-colors'><RiEarthFill className='text-primary' />Red sociales</Link>
+            <button to="/admin/users " className=' flex items-center justify-between gap-4  py-2 px-4 rounded-lg hover:bg-secondary-900 transition-colors'>
+              <span className='flex items-center gap-4'>
+                <RiEarthFill className='text-primary' />Red sociales
+              </span>
+              <RiArrowDropDownLine  />
+            </button>
 
             {/* submenu           */}
-            <ul className='mt-4 text-xs'>
+            <ul className='my-2 text-xs'>
               <li>
-                <Link to="/" className='py-2 px-4 border-l border-gray-500 ml-6 block relative before:w-3 before:h-3 before:absolute before:bg-primary before:rounded-full before:-left-2 before:top-1/2 before:-translate-y-1/2 before:border-2 before:border-secondary-100'>Post Red Social</Link>
+                <Link to="/" className='py-2 px-4 border-l border-gray-500 ml-6 block relative before:w-3 before:h-3 before:absolute before:bg-primary before:rounded-full before:-left-[6.4px] before:top-1/2 before:-translate-y-1/2 before:border-4 before:border-secondary-100 text-white'>Post Red Social</Link>
               </li>
-              <li>
-                <Link to="/" className='py-2 px-4 border-l border-gray-500 ml-6 block  '>Estadisticas</Link>
-              </li>
-              <li>
-                <Link to="/" className='py-2 px-4 border-l border-gray-500 ml-6 block'>Perfiles</Link>
-              </li>
-            </ul>
 
+              <li>
+                <Link to="/" className='py-2 px-4 border-l border-gray-500 ml-6 block relative before:w-3 before:h-3 before:absolute before:bg-gray-500 before:rounded-full before:-left-[6.4px] before:top-1/2 before:-translate-y-1/2 before:border-4 before:border-secondary-100'>Estadisticas</Link>
+              </li>
+              <li>
+                <Link to="/" className='py-2 px-4 border-l border-gray-500 ml-6 block relative before:w-3 before:h-3 before:absolute before:bg-gray-500 before:rounded-full before:-left-[6.4px] before:top-1/2 before:-translate-y-1/2 before:border-4 before:border-secondary-100'>Perfiles</Link>
+              </li>
+
+            </ul>
           </li>
 
-      
+
           {/* submenu */}
           <li>
             <Link to="/admin/users " className=' flex items-center gap-4 py-2 px-4 rounded-lg hover:bg-secondary-900 transition-colors'><RiMessage2Fill className='text-primary' />Mensajes</Link>
