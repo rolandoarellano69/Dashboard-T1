d9583b704ada1b706af7a898d9fcabef21d993ca
Rolando Arellano <rolandoarellano9869@hotmail.com>
2023-04-21 12:57:46 -0700

    update header v1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { RiArrowDownSLine, RiNotification3Line, RiSettings3Line, RiLogoutBoxRLine, RiThumbUpLine,RiChat1Line } from 'react-icons/ri'
+import { RiArrowDownSLine, RiNotification3Line, RiSettings3Line, RiLogoutBoxRLine, RiThumbUpLine, RiChat1Line } from 'react-icons/ri'
 
 import { Menu, MenuItem, MenuButton } from '@szhsin/react-menu'
 import '@szhsin/react-menu/dist/index.css'
@@ -9,17 +9,17 @@ import '@szhsin/react-menu/dist/transitions/slide.css'
 const Header = () => {
   return (
     <header className=' h-[7vh] md:h[10vh] border-b border-secondary-100 p-8 flex items-center justify-end'>
-      <nav className='flex items-center'>
+      <nav className='flex items-center gap-2'>
 
-        <Menu menuButton={<MenuButton className='relative hover:bg-secondary-100 py-2 px-4 rounded-lg transition-colors'>
+        <Menu menuButton={<MenuButton className='relative hover:bg-secondary-100 p-2 rounded-lg transition-colors'>
           <RiNotification3Line />
           <span className='absolute -top-0.5 -right-0.1 bg-primary py-0.5 px-[5px] content-box text-white rounded-full text-[8px] font-bold'>
             2
           </span>
         </MenuButton>} arrow arrowClassName="bg-secondary-100" transition menuClassName="bg-secondary-100 text-white p-4">
-          <h1 className='text-gray-300 text-center font-medium'>Notificaciones</h1>
+          <h1 className='text-gray-300 text-center font-medium'>Notificaciones (2)</h1>
 
-          <hr className='my-2 border-gray-500' />
+          <hr className='my-4 border-gray-500' />
 
           <MenuItem className="p-0 hover:bg-transparent">
 
@@ -29,7 +29,7 @@ const Header = () => {
               <img src="https://img.freepik.com/foto-gratis/retrato-hermoso-mujer-joven-posicion-pared-gris_231208-10760.jpg" className='w-8 h-8 object-cover rounded-full' />
               <div className='text-sm flex flex-col'>
                 <div className='flex items-center justify-between gap-4 '>
-                <span>Rolando Arellano</span><span className='text-[8px]'> 10/10/2023</span>
+                  <span>Rolando Arellano</span><span className='text-[8px]'> 10/10/2023</span>
                 </div>
                 <p className='text-gray-500 text-xs'>Lorem ipsum dolor sit amet...</p>
               </div>
@@ -38,10 +38,10 @@ const Header = () => {
 
           <MenuItem className="p-0 hover:bg-transparent">
             <Link to="/not" className='flex  text-gray-300  items-center py-2 px-4 hover:bg-secondary-900 transition-colors rounded-lg gap-4'>
-                <RiThumbUpLine className='bg-blue-100 p-2 text-blue-700 box-content rounded-full'/>
+              <RiThumbUpLine className='bg-blue-100 p-2 text-blue-700 box-content rounded-full' />
               <div className='text-sm flex flex-col'>
                 <div className='flex items-center justify-between gap-4 '>
-                <span>Nuevo Like</span><span className='text-[8px]'> 10/10/2023</span>
+                  <span>Nuevo Like</span><span className='text-[8px]'> 10/10/2023</span>
                 </div>
                 <p className='text-gray-500 text-xs'>Lorem ipsum dolor sit amet...</p>
               </div>
@@ -52,14 +52,25 @@ const Header = () => {
             <Link to="/not" className='flex  text-gray-300  items-center py-2 px-4 hover:bg-secondary-900 transition-colors rounded-lg gap-4'>
 
 
-           <RiChat1Line className='bg-green-200 p-2 text-primary box-content rounded-full'/>
+              <RiChat1Line className='bg-green-200 p-2 text-primary box-content rounded-full' />
               <div className='text-sm flex flex-col'>
                 <div className='flex items-center justify-between gap-4 '>
-                <span>Nuevo comentario</span><span className='text-[8px]'> 10/10/2023</span>
+                  <span>Nuevo comentario</span><span className='text-[8px]'> 10/10/2023</span>
                 </div>
-                <p className='text-gray-500 text-xs'>Lorem ipsum dolor sit amet...</p>
+                <p className='text-gray-500 text-xs'>Lorem ipsum dolor sit amet...
+
+                </p>
               </div>
             </Link>
+          </MenuItem>
+
+          <hr className='my-2 border-gray-500' />
+          <MenuItem className="p-0   hover:bg-transparent justify-center cursor-default">
+            <Link to="/" className='text-gray-500 hover:text-white  transition-colors text-sm  p-1 rounded-lg' >
+              Todas las Notificaciones
+
+            </Link>
+
           </MenuItem>
 
         </Menu>
@@ -81,7 +92,8 @@ const Header = () => {
               </div>
             </Link>
           </MenuItem>
-          <hr className='my-2 border-gray-500' />
+
+          <hr className='my-6 border-gray-500' />
 
           <MenuItem className="p-0 hover:bg-transparent">
             <Link to="/configuracion" className=' text-sm rounded-lg transition-colors text-gray-300 hover:bg-secondary-900 flex items-center gap-x-4 py-2 px-6 flex-1'>
